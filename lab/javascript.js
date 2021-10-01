4aa8bb0173b62d9339c744a41abb0ece288cb950
guillermo.mediero@ikasle.egibide.org <2gdaw01@ARRIAGAINFO.LOCAL>
2021-10-01 11:00:23 +0200

    Pagina web pc

--- lab/javascript.js
+++ lab/javascript.js
@@ -1,4 +1,15 @@
 function mostrar() {
-    document.getElementById("menu-vertical").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
+    document.getElementById("menu-vertical").style.width = "30%";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
 }
+
+
+function ocultar() {
+    document.getElementById("menu-vertical").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}
+
